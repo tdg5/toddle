4cbb22630f892cbe8daff42e347395b1739f477d
Danny Guinther <dannyguinther@gmail.com>
2022-03-25 18:15:07 -0400

    Move word list into its own file

--- src/features/speller/spellerSlice.js
+++ src/features/speller/spellerSlice.js
@@ -1,60 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const wordList = [
-  {
-    imagePath: '/images/words/bat.png',
-    name: 'bat',
-  }, {
-    imagePath: '/images/words/bed.png',
-    name: 'bed',
-  }, {
-    imagePath: '/images/words/car.png',
-    name: 'car',
-  }, {
-    imagePath: '/images/words/cat.png',
-    name: 'cat',
-  }, {
-    imagePath: '/images/words/dog.png',
-    name: 'dog',
-  }, {
-    imagePath: '/images/words/fan.png',
-    name: 'fan',
-  }, {
-    imagePath: '/images/words/fire.png',
-    name: 'fire',
-  }, {
-    imagePath: '/images/words/frog.png',
-    name: 'frog',
-  }, {
-    imagePath: '/images/words/ham.png',
-    name: 'ham',
-  }, {
-    imagePath: '/images/words/jam.png',
-    name: 'jam',
-  }, {
-    imagePath: '/images/words/log.png',
-    name: 'log',
-  }, {
-    imagePath: '/images/words/man.png',
-    name: 'man',
-  }, {
-    imagePath: '/images/words/nose.png',
-    name: 'nose',
-  }, {
-    imagePath: '/images/words/pan.png',
-    name: 'pan',
-  }, {
-    imagePath: '/images/words/shoe.png',
-    name: 'shoe',
-  },
-];
+import { words } from '../../shared/words';
 
 const initialState = {
   keyboardLetters: [],
   status: 'choose-word',
   targetWord: null,
   wordInProgress: null,
-  wordList: wordList,
+  wordList: words,
 };
 
 const getKeyboardLetters = function(wordList, word) {
---
+++ src/shared/words.js
@@ -0,0 +1,48 @@
+export const words = [
+  {
+    imagePath: '/images/words/bat.png',
+    name: 'bat',
+  }, {
+    imagePath: '/images/words/bed.png',
+    name: 'bed',
+  }, {
+    imagePath: '/images/words/car.png',
+    name: 'car',
+  }, {
+    imagePath: '/images/words/cat.png',
+    name: 'cat',
+  }, {
+    imagePath: '/images/words/dog.png',
+    name: 'dog',
+  }, {
+    imagePath: '/images/words/fan.png',
+    name: 'fan',
+  }, {
+    imagePath: '/images/words/fire.png',
+    name: 'fire',
+  }, {
+    imagePath: '/images/words/frog.png',
+    name: 'frog',
+  }, {
+    imagePath: '/images/words/ham.png',
+    name: 'ham',
+  }, {
+    imagePath: '/images/words/jam.png',
+    name: 'jam',
+  }, {
+    imagePath: '/images/words/log.png',
+    name: 'log',
+  }, {
+    imagePath: '/images/words/man.png',
+    name: 'man',
+  }, {
+    imagePath: '/images/words/nose.png',
+    name: 'nose',
+  }, {
+    imagePath: '/images/words/pan.png',
+    name: 'pan',
+  }, {
+    imagePath: '/images/words/shoe.png',
+    name: 'shoe',
+  },
+];
